2b155c5bcceef774d45b43e2e3476665824f1742
رضائی - محمد <rezaee-moha@tehran.iri>
2023-02-17 21:33:56 +0330

    install rollup packages and config them

---
+++ packages/react/lib/atoms/Button/Button.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Button = ({ label }) => {
+    return (React.createElement(React.Fragment, null,
+        React.createElement("button", null, label)));
+};
+
+export { Button as default };
+//# sourceMappingURL=Button.js.map
