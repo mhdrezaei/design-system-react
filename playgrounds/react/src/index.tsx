d73cfb934a0519dfa0a32d70025acc1cdde315ed
رضائی - محمد <rezaee-moha@tehran.iri>
2023-02-18 12:15:25 +0330

    create an environment for test react components

---
+++ playgrounds/react/src/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDom from "react-dom"
+
+import { Button } from "@ds.e/react"
+
+ReactDom.render(
+    <Button label='Example Buton' ></Button>,
+    document.querySelector('#root')
+)
